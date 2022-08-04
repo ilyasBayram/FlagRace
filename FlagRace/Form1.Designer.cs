9304f10ea0db2e9b7a87f6645f4468335d151d42
ilyasBayram <ilyas.bayram@hotmail.com>
2022-08-04 21:15:46 +0300

    buuton text is added.

--- FlagRace/Form1.Designer.cs
+++ FlagRace/Form1.Designer.cs
@@ -31,10 +31,15 @@ namespace FlagRace
         {
             this.cmBoxConitinents = new System.Windows.Forms.ComboBox();
             this.lblContinent = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
             this.pctBoxFlag = new System.Windows.Forms.PictureBox();
             this.btnA = new System.Windows.Forms.Button();
             this.btnB = new System.Windows.Forms.Button();
+            this.grpBoxGame = new System.Windows.Forms.GroupBox();
+            this.lblRealCorrectAnswer = new System.Windows.Forms.Label();
+            this.lblCorrectAnswer = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pctBoxFlag)).BeginInit();
+            this.grpBoxGame.SuspendLayout();
             this.SuspendLayout();
             // 
             // cmBoxConitinents
@@ -51,24 +56,34 @@ namespace FlagRace
             "Europe",
             "Antartica",
             ""});
-            this.cmBoxConitinents.Location = new System.Drawing.Point(206, 62);
+            this.cmBoxConitinents.Location = new System.Drawing.Point(18, 130);
             this.cmBoxConitinents.Name = "cmBoxConitinents";
-            this.cmBoxConitinents.Size = new System.Drawing.Size(396, 28);
+            this.cmBoxConitinents.Size = new System.Drawing.Size(218, 28);
             this.cmBoxConitinents.TabIndex = 0;
             // 
             // lblContinent
             // 
             this.lblContinent.AutoSize = true;
             this.lblContinent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblContinent.Location = new System.Drawing.Point(15, 62);
+            this.lblContinent.Location = new System.Drawing.Point(19, 83);
             this.lblContinent.Name = "lblContinent";
-            this.lblContinent.Size = new System.Drawing.Size(127, 29);
+            this.lblContinent.Size = new System.Drawing.Size(211, 29);
             this.lblContinent.TabIndex = 1;
-            this.lblContinent.Text = "Continent :";
+            this.lblContinent.Text = " Choose Continent";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(24, 389);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(179, 63);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // pctBoxFlag
             // 
-            this.pctBoxFlag.Location = new System.Drawing.Point(263, 183);
+            this.pctBoxFlag.Location = new System.Drawing.Point(142, 87);
             this.pctBoxFlag.Name = "pctBoxFlag";
             this.pctBoxFlag.Size = new System.Drawing.Size(300, 188);
             this.pctBoxFlag.TabIndex = 2;
@@ -76,9 +91,9 @@ namespace FlagRace
             // 
             // btnA
             // 
-            this.btnA.Location = new System.Drawing.Point(168, 457);
+            this.btnA.Location = new System.Drawing.Point(55, 350);
             this.btnA.Name = "btnA";
-            this.btnA.Size = new System.Drawing.Size(189, 54);
+            this.btnA.Size = new System.Drawing.Size(189, 63);
             this.btnA.TabIndex = 3;
             this.btnA.Text = "button1";
             this.btnA.UseVisualStyleBackColor = true;
@@ -86,27 +101,64 @@ namespace FlagRace
             // 
             // btnB
             // 
-            this.btnB.Location = new System.Drawing.Point(460, 457);
+            this.btnB.Location = new System.Drawing.Point(356, 350);
             this.btnB.Name = "btnB";
-            this.btnB.Size = new System.Drawing.Size(189, 54);
+            this.btnB.Size = new System.Drawing.Size(189, 63);
             this.btnB.TabIndex = 4;
             this.btnB.Text = "button2";
             this.btnB.UseVisualStyleBackColor = true;
+            this.btnB.Click += new System.EventHandler(this.btnB_Click);
+            // 
+            // grpBoxGame
+            // 
+            this.grpBoxGame.Controls.Add(this.lblRealCorrectAnswer);
+            this.grpBoxGame.Controls.Add(this.lblCorrectAnswer);
+            this.grpBoxGame.Controls.Add(this.btnB);
+            this.grpBoxGame.Controls.Add(this.btnA);
+            this.grpBoxGame.Controls.Add(this.pctBoxFlag);
+            this.grpBoxGame.Location = new System.Drawing.Point(253, 39);
+            this.grpBoxGame.Name = "grpBoxGame";
+            this.grpBoxGame.Size = new System.Drawing.Size(581, 564);
+            this.grpBoxGame.TabIndex = 6;
+            this.grpBoxGame.TabStop = false;
+            this.grpBoxGame.Text = "Game Area";
+            // 
+            // lblRealCorrectAnswer
+            // 
+            this.lblRealCorrectAnswer.AutoSize = true;
+            this.lblRealCorrectAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblRealCorrectAnswer.Location = new System.Drawing.Point(364, 492);
+            this.lblRealCorrectAnswer.Name = "lblRealCorrectAnswer";
+            this.lblRealCorrectAnswer.Size = new System.Drawing.Size(23, 25);
+            this.lblRealCorrectAnswer.TabIndex = 6;
+            this.lblRealCorrectAnswer.Text = "0";
+            // 
+            // lblCorrectAnswer
+            // 
+            this.lblCorrectAnswer.AutoSize = true;
+            this.lblCorrectAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCorrectAnswer.Location = new System.Drawing.Point(186, 492);
+            this.lblCorrectAnswer.Name = "lblCorrectAnswer";
+            this.lblCorrectAnswer.Size = new System.Drawing.Size(163, 25);
+            this.lblCorrectAnswer.TabIndex = 5;
+            this.lblCorrectAnswer.Text = "Correct Answer  :";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(846, 620);
-            this.Controls.Add(this.btnB);
-            this.Controls.Add(this.btnA);
-            this.Controls.Add(this.pctBoxFlag);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(140)))), ((int)(((byte)(163)))));
+            this.ClientSize = new System.Drawing.Size(858, 616);
+            this.Controls.Add(this.grpBoxGame);
+            this.Controls.Add(this.btnStart);
             this.Controls.Add(this.lblContinent);
             this.Controls.Add(this.cmBoxConitinents);
             this.Name = "Form1";
             this.Text = "Flag Race";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pctBoxFlag)).EndInit();
+            this.grpBoxGame.ResumeLayout(false);
+            this.grpBoxGame.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,9 +168,13 @@ namespace FlagRace
 
         private System.Windows.Forms.ComboBox cmBoxConitinents;
         private System.Windows.Forms.Label lblContinent;
+        private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.PictureBox pctBoxFlag;
         private System.Windows.Forms.Button btnA;
         private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.GroupBox grpBoxGame;
+        private System.Windows.Forms.Label lblRealCorrectAnswer;
+        private System.Windows.Forms.Label lblCorrectAnswer;
     }
 }
 
--- FlagRace/Form1.cs
+++ FlagRace/Form1.cs
@@ -15,46 +15,75 @@ namespace FlagRace
 {
     public partial class Form1 : Form
     {
-        Random random = new Random();
+        Random randomFile = new Random();
+
+        Random randomButtonText = new Random();
 
         ArrayList usedNumbers = new ArrayList();
+
 
         int numberReal;
 
         int numberWrong;
 
+        int countCorrectAnswer;
+
+        int countTotalQuestion;
+
         string continent;
 
-        void getQuestion()
+        string indexReal;
+
+        string indexFake;
+
+        string[] files;
+
+
+        void realNumberCreat()
         {
-            
-            continent = cmBoxConitinents.Text;
+            numberReal = randomFile.Next(files.Length);
+        }
 
-            string[] files = Directory.GetFiles(string.Format(@"C:\Users\ilyas.bayram\source\repos\FlagRace\FlagRace\Image\{0}", continent));
+        void score()
+        {
+            ;
+            if ((sender as Button).Text==)
+            {
 
+            }
+        }
+
+        void fakeNumberCreat()
+        {
+            numberWrong = randomFile.Next(files.Length);
+        }
+
+        void getQuestion(string continent, string [] files)
+        {
+             
             if (usedNumbers.Count == files.Length)
 
                 usedNumbers.Clear();
 
+            realNumberCreat();
 
-            numberReal = random.Next(files.Length);
+            fakeNumberCreat();
+
+            while (usedNumbers.Contains(numberReal) == true)
+            {
+                realNumberCreat();
+            }
+
+            while (numberReal==numberWrong)
+            {
+                fakeNumberCreat();
+            }
 
             usedNumbers.Add(numberReal);
 
-            numberWrong = random.Next(files.Length);
+            indexReal = Path.GetFileNameWithoutExtension(files[numberReal]);
 
-            while (true)
-            {
-                if (usedNumbers.Contains(numberReal) == true || numberReal==numberRea)
-                {
-                    numberReal = random.Next(files.Length);
-                }
-                
-            }
-
-            string indexReal = Path.GetFileNameWithoutExtension(files[numberReal]);
-
-            string indexfake = Path.GetFileNameWithoutExtension(files[numberWrong]);
+             indexFake = Path.GetFileNameWithoutExtension(files[numberWrong]);
 
             Image imgReal = Image.FromFile(files[numberReal]);
 
@@ -62,8 +91,17 @@ namespace FlagRace
 
             pctBoxFlag.SizeMode = PictureBoxSizeMode.StretchImage;
 
-            btnA.Text = indexReal;
-            btnB.Text = indexfake;
+            if(randomButtonText.Next(0,2)==0)
+            {
+                btnA.Text = indexReal;
+                btnB.Text = indexFake;
+            }
+            else
+            {
+                btnA.Text = indexFake;
+                btnB.Text = indexReal;
+            }
+           
 
         }
         public Form1()
@@ -73,14 +111,74 @@ namespace FlagRace
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            cmBoxConitinents.Text = "All World";
+            cmBoxConitinents.Text = "Europe";
+
+            btnA.Visible = false;
+
+            btnB.Visible = false;
+
         }
 
         private void btnA_Click(object sender, EventArgs e)
         {
-            getQuestion();
+
+            lblRealCorrectAnswer.Text = btnA.Text == indexReal ? Convert.ToString(countCorrectAnswer += 1) : Convert.ToString(countCorrectAnswer);
+
+            getQuestion(continent, files);
+
+            
 
         }
 
+        private void btnB_Click(object sender, EventArgs e)
+        {
+
+            lblRealCorrectAnswer.Text = btnB.Text == indexReal ? Convert.ToString(countCorrectAnswer += 1) : Convert.ToString(countCorrectAnswer);
+
+            getQuestion(continent, files);
+
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (btnStart.Text =="Start")
+            {
+                btnStart.Text = "Exit";
+
+                cmBoxConitinents.Enabled = false;
+
+                btnA.Visible = true;
+
+                btnB.Visible = true;
+
+                pctBoxFlag.Visible = true;
+
+                continent = cmBoxConitinents.Text;
+
+                files = Directory.GetFiles(string.Format(@"C:\Users\ilyas.bayram\source\repos\FlagRace\FlagRace\Image\{0}", continent));
+
+                getQuestion(continent, files);
+            }
+            else if(btnStart.Text == "Exit")
+            {
+                btnStart.Text = "Start";
+
+                cmBoxConitinents.Enabled = true;
+
+                btnA.Visible = false;
+
+                btnB.Visible = false;
+
+                pctBoxFlag.Visible = false;
+
+                usedNumbers.Clear();
+            }
+            
+
+           
+        }
+
+    
     }
 }
